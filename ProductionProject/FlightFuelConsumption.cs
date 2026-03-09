f8ef15193aa7063922f81261cd5e7408e2433705
JTew27 <j.tew2259@student.leedsbeckett.ac.uk>
2026-03-09 20:26:39 +0000

    starting to implement fuel consumption by passing in the relevant info derived from the api endpoint so it can be used in this class. defined the use of csv helper so that it can be used

---
+++ ProductionProject/FlightFuelConsumption.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace ProductionProject
+{
+    public class FlightFuelConsumption
+    {
+        flightsInfo fInfo = new flightsInfo();
+
+
+
+        public FlightFuelConsumption()
+        {
+            //flight info from api that could be useful
+
+            string callsign = this.fInfo.callsign;
+            double speed = this.fInfo.velocity;
+            double baroAltitude = this.fInfo.baro_altitude;
+            double geoAltitude = this.fInfo.geo_altitude;
+            double verticalRate = this.fInfo.vertical_rate;
+            int category = this.fInfo.category;
+        }
+
+    }
+}
