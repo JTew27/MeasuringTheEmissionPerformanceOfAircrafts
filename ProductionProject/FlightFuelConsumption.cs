66ed514ec1e82f30ed6488dfd6a7d280f84f2afd
JTew27 <j.tew2259@student.leedsbeckett.ac.uk>
2026-03-09 22:19:20 +0000

    using csv helper i can read the whole csv file and then search for a specifc flights typecode meaning i can cross reference information correctly in order for improved calculations. aim to use to get typecode to cross reference with my information for each engines fuel flow

--- ProductionProject/FlightFuelConsumption.cs
+++ ProductionProject/FlightFuelConsumption.cs
@@ -1,30 +1,73 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CsvHelper;
 
 namespace ProductionProject
 {
+
     public class FlightFuelConsumption
     {
-        flightsInfo fInfo = new flightsInfo();
+        flightsInfo fInfo;
 
 
 
-        public FlightFuelConsumption()
+        public FlightFuelConsumption(flightsInfo fInfo)
         {
-            //flight info from api that could be useful
 
+            this.fInfo = fInfo;
+
+            //flight info from api that could be useful
+            string icao24 = this.fInfo.icao24;
             string callsign = this.fInfo.callsign;
             double speed = this.fInfo.velocity;
             double baroAltitude = this.fInfo.baro_altitude;
             double geoAltitude = this.fInfo.geo_altitude;
             double verticalRate = this.fInfo.vertical_rate;
             int category = this.fInfo.category;
+
+            string typecode = GetTypecodeByIcao(icao24);
+            Debug.WriteLine($"Typecode: {typecode}");
         }
 
+
+        public string GetTypecodeByIcao(string icao24)
+        {
+
+
+            string path = @"C:\Users\ianct\Downloads\aircraft-database-complete-2025-08.csv";
+
+            using (var reader = new StreamReader(path))
+            using (var aircraftDatabase = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+
+                aircraftDatabase.Read();
+                aircraftDatabase.ReadHeader();
+
+                while (aircraftDatabase.Read())
+                {
+                    string id = aircraftDatabase.GetField("'icao24'");
+
+                    if (id == icao24.Trim())
+                    {
+                        string typecode = aircraftDatabase.GetField("'typecode'")?.Trim('\'');
+                        Debug.WriteLine($"typecode: {typecode} for {id} icao number ");
+                        return typecode;
+                    }
+                   // string typecode = aircraftDatabase.GetField("'typecode'");
+
+                   
+                }
+               
+            }
+
+            return null;
+
+        }
     }
 }
